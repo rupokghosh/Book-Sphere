1e915de14109ad4e7d2cac00b3cd783042487650
rupokghosh <rupokghosh291@gmail.com>
2024-04-23 08:37:17 -0700

    add tailwind & components to hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import HeroSection from "../components/HeroSection";
+import TrendingBooks from "../components/TrendingBooks";
+import Categories from "../components/Categories";
+import BestSellersThisWeek from "../components/BestSellersThisWeek";
+import LatestReleases from "../components/LatestReleases";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSection />
+      <TrendingBooks />
+      <Categories />
+      <BestSellersThisWeek />
+      <LatestReleases />
+    </div>
+  );
 };
 
 export default Home;
