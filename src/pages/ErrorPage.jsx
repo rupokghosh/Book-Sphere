cb845582710b6ef627e397a4aa80c63f486ec533
rupokghosh <rupokghosh291@gmail.com>
2024-04-30 13:41:13 -0700

    add main content

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return <div>
+    <img src="" alt="" />
+    <h1>Something went wrong!</h1>
+    <h4>Please refresh the page or try again later</h4>
+    <Link>Go back Home</Link>
+  </div>;
 };
 
 export default ErrorPage;
