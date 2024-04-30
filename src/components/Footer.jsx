bb159860678ec53f03748e457f869314d7f2b50d
rupokghosh <rupokghosh291@gmail.com>
2024-04-30 14:51:08 -0700

    add footer content & styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
+import githubLogo from "../assets/GITHUBLOGO.png";
+
 const Footer = () => {
   return (
-    <div>
-      <div>a project by rupok</div>
-      <div>
-        <button>Youtube</button>
-        <button>Github</button>
-      </div>
-      <div>design inspired by eduarda mirelly</div>
+    <div className="flex justify-between items-center pt-4 mt-2 mx-8 px-8 gap-16 border-2 border-t-slate-900 border-b-0 border-r-0 border-l-0 border-dashed">
+      <div> • A project by Rupok</div>
+      <a href="https://github.com/rupokghosh/Book-Sphere">
+        <img src={githubLogo} alt="" />
+      </a>
+      <div> • design inspired by eduarda mirelly</div>
     </div>
   );
 };
