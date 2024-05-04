b20eb6638a7263cb5d9df8d4c0bd2c94632b0096
rupokghosh <rupokghosh291@gmail.com>
2024-05-04 16:11:33 -0700

    finish component designs

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,36 +1,31 @@
 import bookCover from "../assets/bookCover.jpg";
 import "../index.css";
+import Search from "./Search";
 
-const HeroSection = () => {
-  return (
-    <div className="flex justify-evenly m-4">
-      <div className="textAndSearch flex flex-col justify-evenly items-center bg-blue gap-4">
-        <div className="text">
-          <h1 className="text-3xl">
-            What book is on <br></br>your mind today?
-          </h1>
-          <p>Explore our library and find your next read. </p>
-        </div>
-        <div className="search flex flex-col gap-6 justify-start items-start">
-          <input
-            type="text"
-            placeholder="Search..."
-            className=" customShadow rounded-full border border-zinc-950 px-20 py-1"
-          />
-          <button className="rounded-full bg-beige-300 hover:bg-beige-500 border border-zinc-950 px-4 py-1">
-            explore
-          </button>
-        </div>
+const HeroSection = () => (
+  <div className="flex justify-evenly m-4">
+    <div className="textAndSearch flex flex-col justify-evenly items-center bg-blue gap-4">
+      <div className="text">
+        <h1 className="text-3xl">
+          What book is on <br></br>your mind today?
+        </h1>
+        <p>Explore our library and find your next read. </p>
       </div>
-      <div className="bigPicture bg-green flex justify-center align-center">
-        <img
-          src={bookCover}
-          alt="books"
-          className="object-cover w-64 h-64 rounded-md customShadow2 m-8"
-        />
+      <div className="search flex flex-col gap-6 justify-start items-start">
+        <Search />
+        <button className="rounded-full bg-beige-300 hover:bg-beige-500 border border-zinc-950 px-4 py-1">
+          explore
+        </button>
       </div>
     </div>
-  );
-};
+    <div className="bigPicture bg-green flex justify-center align-center">
+      <img
+        src={bookCover}
+        alt="books"
+        className="object-cover w-64 h-64 rounded-md customShadow2 m-8"
+      />
+    </div>
+  </div>
+);
 
 export default HeroSection;
--- src/components/TrendingBooks.jsx
+++ src/components/TrendingBooks.jsx
@@ -7,12 +7,16 @@ import itendswithus from "../assets/itendswithus copy.jpg";
 
 const TrendingBooks = () => {
   return (
-    <div className="flex justify-evenly items-center gap-6 mt-4 mx-64">
-      <Card cover={meditations} price="10.99" />
-      <Card cover={ikigai} price="12.99" />
-      <Card cover={mdw} price="20.99" />
-      <Card cover={aliabdaal} price="15.99" />
-      <Card cover={itendswithus} price="29.99" />
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="bold text-2xl m-8">Trending Books</h1>
+
+      <div className="flex justify-evenly items-center mx-64 gap-6 ">
+        <Card cover={meditations} price="10.99" />
+        <Card cover={ikigai} price="12.99" />
+        <Card cover={mdw} price="20.99" />
+        <Card cover={aliabdaal} price="15.99" />
+        <Card cover={itendswithus} price="29.99" />
+      </div>
     </div>
   );
 };
