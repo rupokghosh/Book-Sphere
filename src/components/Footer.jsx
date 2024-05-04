1518eaddcadffa8217f98a6dfc4208dcb42036b1
rupokghosh <rupokghosh291@gmail.com>
2024-05-04 15:23:08 -0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import githubLogo from "../assets/GITHUBLOGO.png";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center pt-4 mt-2 mx-8 px-8 gap-16 border-2 border-t-slate-900 border-b-0 border-r-0 border-l-0 border-dashed">
-      <div> • A project by Rupok</div>
+    <div className="flex justify-between items-center pt-4 mx-8 px-8 mb-4 gap-16 border-2 border-t-slate-900 border-b-0 border-r-0 border-l-0 border-dashed">
+      <div className="text-xl"> • A project by Rupok</div>
       <a href="https://github.com/rupokghosh/Book-Sphere">
         <img src={githubLogo} alt="" />
       </a>
-      <div> • design inspired by eduarda mirelly</div>
+      <div className="texl-xl"> • Design inspired by eduarda mirelly</div>
     </div>
   );
 };
