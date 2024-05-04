651819e25ba77a7225d07b5c8961aec02c615675
rupokghosh <rupokghosh291@gmail.com>
2024-05-03 23:41:06 -0700

    design trending books

--- src/components/TrendingBooks.jsx
+++ src/components/TrendingBooks.jsx
@@ -1,5 +1,15 @@
+import Card from "./Card.jsx";
+
 const TrendingBooks = () => {
-  return <div>TrendingBooks</div>;
+  return (
+    <div className="flex justify-center items-center gap-6 mx-64 my-4 p-8 border-2 rounded-lg border-dashed">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default TrendingBooks;
