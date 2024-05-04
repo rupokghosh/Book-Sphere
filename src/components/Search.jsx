e5d7a4394f94dd49f32e5973981d62e4740f839e
rupokghosh <rupokghosh291@gmail.com>
2024-05-04 15:37:58 -0700

    add search component in separate file

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+const Search = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search..."
+      className=" customShadow rounded-full border border-zinc-950 px-20 py-1"
+    />
+  );
+};
+
+export default Search;
