d9fef15440bb7eca40a6f1a0c44b45242dd05dc0
rupokghosh <rupokghosh291@gmail.com>
2024-05-08 11:06:39 -0700

    update card props and functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,19 @@ import bookMark from "../assets/BOOKMARK_SIMPLE.png";
 import shoppingCart from "../assets/SHOPPING_CART.png";
 
 // eslint-disable-next-line react/prop-types
-const Card = ({ cover, price }) => {
+const Card = ({ cover, price, title }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <img
         className="object-cover rounded-lg shadow-xl shadow-gray-900 w-48 h-64 mb-2"
-        src={cover}
+        src={
+          cover
+            ? "https://covers.openlibrary.org/b/id/" + cover + "-L.jpg"
+            : "https://dummyimage.com/180x190/dedede/3b3b3b&text=Image+Not+Available"
+        }
         alt=""
       />
+      <h1 className="text-md text-center font-bold">{title}</h1>
       <div className="flex justify-center items-center gap-3">
         <div className="border border-slate-900 bg-beige-300 rounded-lg px-8 py-1">
           {`$ ${price}`}
