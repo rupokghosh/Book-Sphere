98318983fb8f72d981a4e970701a9604fb0aac13
rupokghosh <rupokghosh291@gmail.com>
2024-05-08 11:51:54 -0700

    update books

--- src/components/BestSellersThisWeek.jsx
+++ src/components/BestSellersThisWeek.jsx
@@ -1,24 +1,24 @@
 import Card from "./Card";
-import portlands from "../assets/portlands.jpg";
-import debt from "../assets/debt.jpg";
-import elon from "../assets/elon.jpg";
-import fiction from "../assets/fiction.jpg";
-import sapiens from "../assets/sapiens.jpg";
 
 const BestSellersThisWeek = () => {
   const Books = [
-    { cover: portlands, price: "9.99" },
-    { cover: debt, price: "12.99" },
-    { cover: elon, price: "15.99" },
-    { cover: sapiens, price: "18.99" },
-    { cover: fiction, price: "6.99" },
+    { title: "Elon Musk: Enterpreneur", cover: 13825519, price: "9.99" },
+    { title: "Crime and Punishment", cover: 11227994, price: "12.99" },
+    { title: "Sapiens", cover: 8634250, price: "15.99" },
+    { title: "Feel Good Productivity", cover: 14557462, price: "18.99" },
+    { title: "The Outsider", cover: 8315581, price: "6.99" },
   ];
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className="text-2xl bold m-8">Our best sellers this week</h2>
       <div className="flex justify-evenly items-center mx-64 gap-6 ">
         {Books.map((book) => (
-          <Card cover={book.cover} price={book.price} key={book.cover} />
+          <Card
+            cover={book.cover}
+            price={book.price}
+            key={book.cover}
+            title={book.title}
+          />
         ))}
       </div>
     </div>
